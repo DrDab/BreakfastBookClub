e91a5fe32c701c9207b611a333f6e080bd1f8120
Zaynab Khan <zaynabk@uw.edu>
2023-02-06 19:16:39 -0800

    Set up firebase for sign up.

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -6,17 +6,23 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 import EggAltIcon from '@mui/icons-material/EggAlt';
-import { Link as RouterLink } from "react-router-dom";
-//import { createUserWithEmailAndPassword } from "firebase/auth";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { auth, register } from "../FirebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function SignUp() {
+  let navigate = useNavigate();
 
-  const [username, setUsername] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
   const signUpUser = () => {
-    console.log("sign up", username);
+    console.log("sign up", email);
+    register(name, email, password);
   }
+
+  // TODO: Edit sign up button to navigate to profile page on click. Wait for firebase to finish register first.
 
   return (
     <Box className="login-signup">
@@ -30,7 +36,14 @@ export default function SignUp() {
           label="Create Username"
           variant="filled"
           size="small"
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          InputProps={{ disableUnderline: true }}
+          label="Enter Email Address"
+          variant="filled"
+          size="small"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
           InputProps={{ disableUnderline: true }}
@@ -38,8 +51,9 @@ export default function SignUp() {
           variant="filled"
           size="small"
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <Button disableElevation onClick={signUpUser} size="small" variant='contained'>
+        <Button disableElevation onClick={signUp} size="small" variant='contained'>
           Sign Up
         </Button>
         <Typography variant="caption">
