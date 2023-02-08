7a0339ceb93e63039dc289d67ad67875938c5d97
Amandaha29 <Amandaha29@gmail.com>
2023-02-08 14:21:23 -0800

    fixed login

--- frontend/src/webapp/pages/LogIn.js
+++ frontend/src/webapp/pages/LogIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -7,28 +8,32 @@ import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 import EggAltIcon from '@mui/icons-material/EggAlt';
 import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { db, auth, logIn } from "../../FirebaseConfig";
-import {useAuthState} from "react-firebase-hooks/auth";
-import { collection, query, where, doc, getDoc } from "firebase/firestore";
-// import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
 export default function LogIn() {
   let navigate = useNavigate();
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [user] = useAuthState(auth);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [isError, setIsError] = React.useState(false);
 
-  const loginUser = () => {
-    logIn(email, password);
-    sessionStorage.setItem('yourUser', JSON.stringify(user.uid));
-    if (user) {
-      navigate("/");
-      window.location.reload();
-    } else {
-      console.log("log in", email);
+
+  const loginUser = async () => {
+    try {
+      const auth = getAuth();
+      await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        sessionStorage.setItem('yourUser', JSON.stringify(user.uid));
+        navigate("/");
+        window.location.reload();
+      })
+    } catch (err) {
+      setIsError(true);
+      setErrorMessage("Login is incorrect");
     }
-  }
+  };
 
   return (
     <Box className="login-signup">
@@ -52,6 +57,10 @@ export default function LogIn() {
           type="password"
           onChange={(e) => setPassword(e.target.value)}
         />
+        {isError &&
+          <Alert severity="error">
+            {errorMessage}
+          </Alert>}
         <Button 
           onClick={loginUser}
           disableElevation 
