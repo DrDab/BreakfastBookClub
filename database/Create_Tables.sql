ac54a84afa9a4b61479ce9b007e30c6e96cfea45
Sanjana J <sanjanaz@uw.edu>
2023-02-09 12:23:33 -0800

    Updated Create_Tables.sql to include the posting date of book posts and updated user_id length

--- database/Create_Tables.sql
+++ database/Create_Tables.sql
@@ -14,34 +14,36 @@ CREATE TABLE books (
 
 -- DROP TABLE book_posts;
 CREATE TABLE book_posts (
-	user_id VARCHAR(20),
+	user_id VARCHAR(50),
 	book_key VARCHAR(20),
+	post_title VARCHAR(100),
 	post VARCHAR(1000),
-	tag VARCHAR(20)
+	tag VARCHAR(20),
+	post_date DATE
 );
 
 -- DROP TABLE friends;
 CREATE TABLE friends (
-	user_id_1 VARCHAR(20),
-	user_id_2 VARCHAR(20)
+	user_id_1 VARCHAR(50),
+	user_id_2 VARCHAR(50)
 ); 
 	
 -- DROP TABLE user_clubs;
 CREATE TABLE user_clubs (
-	user_id VARCHAR(20),
+	user_id VARCHAR(50),
 	book_key VARCHAR (20)
 ); 
 
 -- DROP TABLE saved_books;
 CREATE TABLE saved_books (
-	user_id VARCHAR(20),
+	user_id VARCHAR(50),
 	book_key VARCHAR (20)
 );
 
 -- DROP TABLE sent_recommendations;
 CREATE TABLE sent_recommendations (
-	sender_username VARCHAR(20),
-	recipient_username VARCHAR(20),
+	sender_username VARCHAR(50),
+	recipient_username VARCHAR(50),
 	book_key VARCHAR (20)
 );
 
