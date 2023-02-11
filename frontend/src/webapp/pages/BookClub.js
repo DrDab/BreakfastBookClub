fa23946857ea686c825dcd4b2a51f58f11e31ed4
Amandaha29 <Amandaha29@gmail.com>
2023-02-11 13:29:16 -0800

    book club fake data

--- frontend/src/webapp/pages/BookClub.js
+++ frontend/src/webapp/pages/BookClub.js
@@ -13,14 +13,17 @@ export default function BookClub() {
 
   let bookClubPostsData = [];
   for (let i = 0; i < 20; i++ ) {
-      let user = i % 3 == 0? "Andrea" : i % 3 == 1? "Jocelyn" : "Victor" ;
-      let club = bookData.title;
-      let title = i % 3 == 0? "Spoiler Review" : i % 3 == 1? "Book Recommendations 2023" : "Hot take on chapter 3" ;
-      let content = "Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.";
-      let tag = i % 3 == 0? "Spoiler" : i % 3 == 1? "Theory" : "Discussion" ;
-      let likes = 200;
-      let isLikedByUser = i % 3 == 0? true : i % 3 == 1? false : true;
-      bookClubPostsData.push({user, club, title, content, tag, likes, isLikedByUser})
+    let user = i % 3 == 0? "Andrea" : i % 3 == 1? "Jocelyn" : "Victor";
+    let club = bookData.title;
+    let title = i % 3 == 0? "Spoiler Review" : i % 3 == 1? "New Theory" : "Hot take on chapter 17" ;
+    let content = 
+    i % 3 == 0? "I'm in seventh grade and reading this book really changed my perspective about everyone. Wonder is a book that anyone can read at any age and not think of it as cringe and to heart warming. This is a must read on every ones list. The author puts so much detail and opinion in everyone voice. Wonder is written in a format where most characters have a voice and it really helps you understand their side of the story and their ideas/opinions. Olivia is such a good person in wonder. She sticks with August after everything he's been through, she's always by his side after 27 surgeries. Love this book so much, I could read this book five times a week and not be tired of it after years. Though the book is slightly predictable, but the author makes the rest of the students brutal towards August and near the ends of the book, almost the whole fifth grade is talking with August and being his friend.":
+    i % 3 == 1? "This book is amazing! When I first read this book when Edward left everything became boring to me, so I stopped reading for a week or two. When I started reading again it all started to become better and better and I read up till midnight every day. My suggestion is to keep reading, it will get good! Other than Edward leaving for a bit it was great, read the whole series, you will love them if you love action, vampires, wolves, and romance!":
+    "Being a Harry Potter fan I love Quidditch. Quidditch is a sport in the wizarding world involving a lot of excitement and thrill. Harry Potter and the Goblet of Fire has the most humor and suspense in any book I've ever read. And also, it is the book with the Quidditch World Cup. The most thrilling things happened in this book, involving The Triwizard Tournament and the rising of Lord Voldemort. Besides, it is the first book that ever mentions the existence of Albus Dumbledore's pensieve. I would recommend this book to ages eight to twelve, even though it is definitely suitable for all ages. It is a book that would make you feel you couldn't put it down."
+    let tag = i % 3 == 0? "Spoiler" : i % 3 == 1? "Theory" : "Discussion";
+    let likes = 200;
+    let isLikedByUser = i % 3 == 0? true : i % 3 == 1? false : true;
+    bookClubPostsData.push({user, club, title, content, tag, likes, isLikedByUser})
   }
 
   let membersData = [];
