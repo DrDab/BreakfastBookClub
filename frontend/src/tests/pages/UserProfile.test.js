f35fc3de2ca1b27c9ae91540de539f8cf478de6a
Amandaha29 <Amandaha29@gmail.com>
2023-02-23 22:03:50 -0800

    updated frontend tests

--- frontend/src/tests/pages/UserProfile.test.js
+++ frontend/src/tests/pages/UserProfile.test.js
@@ -2,10 +2,13 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import UserProfile from '../../webapp/pages/UserProfile';
 import {MemoryRouter, Routes, Route  } from 'react-router-dom';
 
-const mockUser = "Amanda";
+const mockUser = {
+  "uid": "EHDvyZymtRSbciB7uXHv1mN5O9r2",
+  "username": "Amanda"
+}
 
 test("User Profile page renders correctly", () => {
-  sessionStorage.setItem('yourUser', JSON.stringify(mockUser));
+  sessionStorage.setItem('loggedinUser', JSON.stringify(mockUser));
   
   const route = "/user-profile/" + mockUser;
   render(
