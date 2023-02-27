d4806fb8a75f81f492fa98d5664cb575dc549640
Sanjana J <sanjanaz@uw.edu>
2023-02-27 13:54:42 -0800

    Added ListFriends file

---
+++ backend/BCAPIServer/src/main/java/routes/friends/ListFriends.java
@@ -0,0 +1,12 @@
+package routes.friends;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class ListFriends implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
