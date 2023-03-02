77d22e49473b054622d9635556b917246bd2c973
DrDab <duvictor514@gmail.com>
2023-03-01 16:16:50 -0800

    Add ListFeed friend post support and backend chrono sorting

--- backend/BCAPIServer/src/main/java/routes/bookclubs/ListFeed.java
+++ backend/BCAPIServer/src/main/java/routes/bookclubs/ListFeed.java
@@ -9,9 +9,15 @@ import com.google.gson.JsonObject;
 import daos.Books;
 import daos.User;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import types.Book;
 import types.BookPost;
 
 import java.util.List;
@@ -50,18 +56,34 @@ public class ListFeed implements Route {
     }
 
     String uid = decodedToken.getUid();
-    User users = new User(uid, sqlConn);
-    Books books = new Books(sqlConn);
+    User user = new User(uid, sqlConn);
 
-    List<String> subscribedBookKeys = users.getSubscribedClubs();
+    List<BookPost> masterList = new ArrayList<>();
+    Set<String> knownIds = new HashSet<>();
     JsonArray postsArr = new JsonArray();
 
-    for (String bookKey : subscribedBookKeys) {
-      List<BookPost> posts = books.listBookPosts(bookKey);
-      JsonArray bookPostsArr = BCGsonUtils.getPostsJsonArrFromList(fbApp, posts);
-      postsArr.addAll(bookPostsArr);
+    for (BookPost post : user.getClubPosts()) {
+      if (!knownIds.contains(post.postId)) {
+        masterList.add(post);
+        knownIds.add(post.postId);
+      }
     }
 
+    List<String> friendUIDs = user.allFriends();
+
+    for (String friendUID : friendUIDs) {
+      User friend = new User(friendUID, sqlConn);
+
+      for (BookPost post : friend.getUserPosts()) {
+        if (!knownIds.contains(post.postId)) {
+          masterList.add(post);
+          knownIds.add(post.postId);
+        }
+      }
+    }
+
+    Collections.sort(masterList, Comparator.comparingLong(p -> p.date));
+    BCGsonUtils.ingestBookPostsListIntoJson(fbApp, masterList, postsArr);
     respJson.add("posts", postsArr);
     respJson.addProperty("status", "success");
     return respJson;
