7c1c62e7869dffc0cd132adbe6f3fb85d985ce77
Sanjana J <sanjanaz@uw.edu>
2023-03-01 16:22:02 -0800

    Fixed routes in BCServerMain

--- backend/BCAPIServer/src/main/java/BCServerMain.java
+++ backend/BCAPIServer/src/main/java/BCServerMain.java
@@ -89,8 +89,8 @@ public class BCServerMain {
     Spark.get("/api/get_members", new GetMembers(fbApp, sqlConn));
 
     Spark.get("/api/list_friends", new ListFriends(fbApp, sqlConn));
-    Spark.get("/api/add_friend", new AddFriend(fbApp, sqlConn));
-    Spark.get("/api/remove_friend", new RemoveFriend(fbApp, sqlConn));
+    Spark.post("/api/add_friend", new AddFriend(fbApp, sqlConn));
+    Spark.post("/api/remove_friend", new RemoveFriend(fbApp, sqlConn));
   }
 
   @SuppressWarnings("deprecation")
